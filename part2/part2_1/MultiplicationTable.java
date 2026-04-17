4e9c262e922996d89330969da0aca17e61467a64
DarinaKochesheva <darinakochesheva930@gmail.com>
2026-04-17 19:35:28 +0300

    complete 2_1

--- part2/part2_1/MultiplicationTable.java
+++ part2/part2_1/MultiplicationTable.java
@@ -5,7 +5,21 @@ public class MultiplicationTable {
     public static void printTable() {
         // TODO: выведите таблицу умножения 1..10, ширина столбца 4.
         // ▼ ВАШ КОД ЗДЕСЬ ▼
+        System.out.print("   |");
+        for (int j = 1; j <= 10; j++) {
+            System.out.printf("%4d", j);
+        }
+        System.out.println();
+        System.out.println("----------------------------------------------");
 
+        // Основная таблица
+        for (int i = 1; i <= 10; i++) {
+            System.out.printf("%2d |", i);
+            for (int j = 1; j <= 10; j++) {
+                System.out.printf("%4d", i * j);
+            }
+            System.out.println();
+        }
         // ▲ КОНЕЦ ВАШЕГО КОДА ▲
     }
 
